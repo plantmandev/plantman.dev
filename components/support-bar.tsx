e1ace2e57e4421d74981c1765014884c227b6045
plantmandev <plantmandev@gmail.com>
2026-03-06 23:22:25 -0700

    removed buy-me-a-coffee

--- components/support-bar.tsx
+++ components/support-bar.tsx
@@ -17,29 +17,11 @@ function FlashcardButton() {
   );
 }
 
-function CoffeeButton() {
-  return (
-    <a
-      className="sb-btn sb-btn--coffee"
-      href="https://buymeacoffee.com/plantmandev"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <img
-        src="/logos/buy-me-a-coffee Logo.png"
-        alt="Buy Me a Coffee"
-        className="sb-logo sb-logo--coffee"
-      />
-      <span className="sb-btn-text">buy me a coffee</span>
-    </a>
-  );
-}
 
 export default function SupportBar() {
   return (
     <div className="sb-root">
       <FlashcardButton />
-      <CoffeeButton />
     </div>
   );
 }
